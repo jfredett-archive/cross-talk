9f15cb537bf13674c0c289666221a178fdce359a
Joe Fredette <jfredett@gmail.com>
2013-04-12 12:33:47 -0400

    wire `include Cross::Talk` to load the listener module

--- lib/cross-talk.rb
+++ lib/cross-talk.rb
@@ -2,12 +2,17 @@ require 'celluloid'
 
 require "cross-talk/version"
 require 'cross-talk/manager'
+require 'cross-talk/listener'
 
 module Cross
   module Talk
     # Access the Event Manager
     def self.manager
       Celluloid::Actor[:manager] ||= Cross::Talk::Manager.new
+    end
+
+    def self.included(base)
+      base.send(:include, Listener)
     end
   end
 end
